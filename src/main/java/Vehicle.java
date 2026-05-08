66a66567cc51c0bac727cffdf90898976b3ec06f
JustinK <productivekeys@gmail.com>
2026-05-08 13:58:19 -0500

    initial commit
    - Building skeleton

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,75 @@
+public class Vehicle {
+    private String vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private int odometer;
+    private double price;
+
+    public Vehicle(String vin, int year, String make, String model, String vehicleType, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
